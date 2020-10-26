48f514dae21591ab334dfd1e0acb25035e5cab8b
rin3124hello <rin3124hello@gmail.com>
2020-10-26 18:43:58 +0900

    1st レビュー対応
    ・登録済みユーザーのログイン表示
    ・ログイン状態でニックネームを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!
+  before_action :move_to_index, except: [:index]
 
   def index
   end
