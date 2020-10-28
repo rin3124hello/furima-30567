37257c165a017e44b1a34657319541625263f0e4
rin3124hello <rin3124hello@gmail.com>
2020-10-28 11:45:04 +0900

    バリデーションの設定
    空や{ id: 1, name: '--' }は保存されない

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :condition, :postage, :region, :day
+
+  validates :title, :text, :category, :price, presence: true
+  validates :condition_id, :postage_id, :region_id, :day_id, numericality: { other_than: 1 }
 end
