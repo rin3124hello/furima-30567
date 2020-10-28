b755d666998a72ee9d84c849e7d72b1b6c660f82
rin3124hello <rin3124hello@gmail.com>
2020-10-28 11:39:36 +0900

    ActiveHashの導入
    ・category, condition, postage, region, dayモデルの作成、編集
    ・Itemモデルとのアソシエーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,21 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
 
   def index
+    @items = Item.order("created_at")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Post.create(item: params[:item])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :category_id, :condition_id, postage_id, region_id, days_id)
   end
 
   def move_to_index
