1c7c20669121c8beba02d8fc78774ffacb627b22
rin3124hello <rin3124hello@gmail.com>
2020-11-01 15:53:18 +0900

    1stレビュー対応

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :move_to_index, except: [:index, :edit]
   before_action :authenticate_user!, only: [:new, :create]
   before_action :set_item, only: [:show, :edit, :update]
 
