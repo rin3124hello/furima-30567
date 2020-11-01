8274e422400391c8ada1dcbdd8417755d583f0c3
rin3124hello <rin3124hello@gmail.com>
2020-11-01 16:22:46 +0900

    2ndレビュー対応

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :edit]
+  before_action :move_to_index, except: [:index]
   before_action :authenticate_user!, only: [:new, :create]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :user_check, only: [:edit]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -19,6 +20,9 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+  
+  def show
+  end
 
   def edit
   end
@@ -29,9 +33,6 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
-  end
-
-  def show
   end
 
   private
@@ -47,6 +48,12 @@ class ItemsController < ApplicationController
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
+    end
+  end
+
+  def user_check
+    if current_user != @item.user_id?
+      redirect_to root_path
     end
   end
 end
